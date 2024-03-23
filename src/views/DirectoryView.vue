1b76eb99ef83eff77a595ee0ba379b753e3c7711
NickDomanti <domanti.nic@gmail.com>
2024-03-23 13:28:30 +0100

    stylized directory

--- src/views/DirectoryView.vue
+++ src/views/DirectoryView.vue
@@ -9,11 +9,24 @@
 
 <style scoped>
 .directory {
+  flex-grow: 1;
   display: flex;
   flex-direction: column;
   gap: 0.5rem;
   border: 1px solid var(--clr-red2);
   padding: 1rem;
   background-color: rgba(0, 0, 0, 0.5);
+}
+
+a {
+  display: block;
+  border: 1px solid var(--clr-red2);
+  padding: 1rem 3rem;
+  will-change: background-color;
+  transition: background-color 100ms;
+}
+
+a:hover {
+  background-color: var(--clr-red3);
 }
 </style>
